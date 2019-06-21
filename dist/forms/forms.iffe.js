42ebec87ac785ed1ef7caef441171d89bea91e20
devowhippit <devonhirth@gmail.com>
2019-06-21 12:07:04 -0400

    Spinner element, form refactor to include watching method and docs

--- dist/forms/forms.iffe.js
+++ dist/forms/forms.iffe.js
@@ -6,25 +6,10 @@ var Forms = (function () {
    * @class
    */
 
-  var Forms = function Forms() {
-    /** Strings can be passed to the constructor */
-    this.STRINGS = Forms.strings;
+  var Forms = function Forms(form) {
+    if (form === void 0) form = false;
+    this.FORM = form;
     return this;
-  };
-  /**
-   * Takes a a string and returns whether or not the string is a valid email
-   * by using native browser validation if available. Otherwise, does a simple
-   * Regex test.
-   * @param {string} email
-   * @return {boolean}
-   */
-
-
-  Forms.prototype.validateEmail = function validateEmail(email) {
-    var input = document.createElement('input');
-    input.type = 'email';
-    input.value = email;
-    return typeof input.checkValidity === 'function' ? input.checkValidity() : /\S+@\S+\.\S+/.test(email);
   };
   /**
    * Map toggled checkbox values to an input.
@@ -59,64 +44,125 @@ var Forms = (function () {
    * See https://developer.mozilla.org/en-US/docs/Learn/HTML/Forms/Form_validation
    * See https://caniuse.com/#feat=form-validation for support
    *
-   * @param{Event}       event The form submission event.
-   * @return {Event/Boolean}     The original event or false if invalid.
+   * @param{Event}       event The form submission event
+   * @return {Class/Boolean}     The form class or false if invalid
    */
 
 
   Forms.prototype.valid = function valid(event) {
-    event.preventDefault();
     var validity = event.target.checkValidity();
-    var elements = event.target.querySelectorAll('input[required="true"]');
+    var elements = event.target.querySelectorAll(Forms.selectors.REQUIRED);
 
     for (var i = 0; i < elements.length; i++) {
       // Remove old messaging if it exists
       var el = elements[i];
-      var container = el.parentNode;
-      var message = container.querySelector('.error-message');
-      container.classList.remove('error');
-
-      if (message) {
-        message.remove();
-      } // If this input valid, skip messaging
-
+      this.reset(el); // If this input valid, skip messaging
 
       if (el.validity.valid) {
         continue;
-      } // Create the new error message.
-
-
-      message = document.createElement('div'); // Get the error message from localized strings.
-
-      if (el.validity.valueMissing && this.STRINGS.VALID_REQUIRED) {
-        message.innerHTML = this.STRINGS.VALID_REQUIRED;
-      } else if (!el.validity.valid && this.STRINGS["VALID_" + el.type.toUpperCase() + "_INVALID"]) {
-        var stringKey = "VALID_" + el.type.toUpperCase() + "_INVALID";
-        message.innerHTML = this.STRINGS[stringKey];
-      } else {
-        message.innerHTML = el.validationMessage;
       }
 
-      message.setAttribute('aria-live', 'polite');
-      message.classList.add('error-message'); // Add the error class and error message.
-
-      container.classList.add('error');
-      container.insertBefore(message, container.childNodes[0]);
+      this.highlight(el);
     }
 
-    return validity ? event : validity;
+    return validity ? this : validity;
   };
   /**
-   * A function to assign pre localized strings to the class.
-   * @param{object} localizedStrings A dictionairy of strings in the format
-   *                                 'VALID_REQUIRED': 'This is required',
-   *                                 'VALID_{{ TYPE }}_INVALID': 'Invalid'
-   * @return {class}                 The object class
+   * Adds focus and blur events to inputs with required attributes
+   * @param {object}formPassing a form is possible, otherwise it will use
+   *                        the form passed to the constructor.
+   * @return{class}       The form class
    */
 
 
-  Forms.prototype.strings = function strings(localizedStrings) {
-    Object.assign(this.STRINGS, localizedStrings);
+  Forms.prototype.watch = function watch(form) {
+    var this$1 = this;
+    if (form === void 0) form = false;
+    this.FORM = form ? form : this.FORM;
+    var elements = this.FORM.querySelectorAll(Forms.selectors.REQUIRED);
+    /** Watch Individual Inputs */
+
+    var loop = function loop(i) {
+      // Remove old messaging if it exists
+      var el = elements[i];
+      el.addEventListener('focus', function () {
+        this$1.reset(el);
+      });
+      el.addEventListener('blur', function () {
+        if (!el.validity.valid) {
+          this$1.highlight(el);
+        }
+      });
+    };
+
+    for (var i = 0; i < elements.length; i++) {
+      loop(i);
+    }
+    /** Submit Event */
+
+
+    this.FORM.addEventListener('submit', function (event) {
+      event.preventDefault();
+
+      if (this$1.valid(event) === false) {
+        return false;
+      }
+
+      this$1.submit(event);
+    });
+    return this;
+  };
+  /**
+   * Removes the validity message and classes from the message.
+   * @param {object}elThe input element
+   * @return{class}     The form class
+   */
+
+
+  Forms.prototype.reset = function reset(el) {
+    var container = el.parentNode;
+    var message = container.querySelector('.' + Forms.classes.ERROR_MESSAGE); // Remove old messaging if it exists
+
+    container.classList.remove(Forms.classes.ERROR_CONTAINER);
+
+    if (message) {
+      message.remove();
+    }
+
+    return this;
+  };
+  /**
+   * Displays a validity message to the user. It will first use any localized
+   * string passed to the class for required fields missing input. If the
+   * input is filled in but doesn't match the required pattern, it will use
+   * a localized string set for the specific input type. If one isn't provided
+   * it will use the default browser provided message.
+   * @param {object}elThe invalid input element
+   * @return{class}     The form class
+   */
+
+
+  Forms.prototype.highlight = function highlight(el) {
+    var container = el.parentNode;
+    var message = container.querySelector('.' + Forms.classes.ERROR_MESSAGE); // Create the new error message.
+
+    message = document.createElement(Forms.markup.ERROR_MESSAGE); // Get the error message from localized strings (if set).
+
+    if (el.validity.valueMissing && Forms.strings.VALID_REQUIRED) {
+      message.innerHTML = Forms.strings.VALID_REQUIRED;
+    } else if (!el.validity.valid && Forms.strings["VALID_" + el.type.toUpperCase() + "_INVALID"]) {
+      var stringKey = "VALID_" + el.type.toUpperCase() + "_INVALID";
+      message.innerHTML = Forms.strings[stringKey];
+    } else {
+      message.innerHTML = el.validationMessage;
+    } // Set aria attributes and css classes to the message
+
+
+    message.setAttribute(Forms.attrs.ERROR_MESSAGE[0], Forms.attrs.ERROR_MESSAGE[1]);
+    message.classList.add(Forms.classes.ERROR_MESSAGE); // Add the error class and error message to the dom.
+
+    container.classList.add(Forms.classes.ERROR_CONTAINER);
+    container.insertBefore(message, container.childNodes[0]);
     return this;
   };
   /**
@@ -129,6 +175,35 @@ var Forms = (function () {
 
 
   Forms.strings = {};
+  /** Placeholder for the submit function */
+
+  Forms.submit = function () {};
+  /** Classes for various containers */
+
+
+  Forms.classes = {
+    'ERROR_MESSAGE': 'error-message',
+    // error class for the validity message
+    'ERROR_CONTAINER': 'error' // class for the validity message parent
+
+  };
+  /** HTML tags and markup for various elements */
+
+  Forms.markup = {
+    'ERROR_MESSAGE': 'div'
+  };
+  /** DOM Selectors for various elements */
+
+  Forms.selectors = {
+    'REQUIRED': '[required="true"]' // Selector for required input elements
+
+  };
+  /** Attributes for various elements */
+
+  Forms.attrs = {
+    'ERROR_MESSAGE': ['aria-live', 'polite'] // Attribute for valid error message
+
+  };
 
   return Forms;
 
--- src/forms/forms.js
+++ src/forms/forms.js
@@ -9,27 +9,10 @@ class Forms {
    * The Form constructor
    * @return {object} The class
    */
-  constructor() {
-    /** Strings can be passed to the constructor */
-    this.STRINGS = Forms.strings;
+  constructor(form = false) {
+    this.FORM = form;
 
     return this;
-  }
-
-  /**
-   * Takes a a string and returns whether or not the string is a valid email
-   * by using native browser validation if available. Otherwise, does a simple
-   * Regex test.
-   * @param {string} email
-   * @return {boolean}
-   */
-  validateEmail(email) {
-    const input = document.createElement('input');
-    input.type = 'email';
-    input.value = email;
-
-    return typeof input.checkValidity === 'function' ?
-      input.checkValidity() : /\S+@\S+\.\S+/.test(email);
   }
 
   /**
@@ -65,62 +48,119 @@ class Forms {
    * See https://developer.mozilla.org/en-US/docs/Learn/HTML/Forms/Form_validation
    * See https://caniuse.com/#feat=form-validation for support
    *
-   * @param  {Event}         event The form submission event.
-   * @return {Event/Boolean}       The original event or false if invalid.
+   * @param  {Event}         event The form submission event
+   * @return {Class/Boolean}       The form class or false if invalid
    */
   valid(event) {
-    event.preventDefault();
-
     let validity = event.target.checkValidity();
-    let elements = event.target.querySelectorAll('input[required="true"]');
+    let elements = event.target.querySelectorAll(Forms.selectors.REQUIRED);
 
     for (let i = 0; i < elements.length; i++) {
       // Remove old messaging if it exists
       let el = elements[i];
-      let container = el.parentNode;
-      let message = container.querySelector('.error-message');
 
-      container.classList.remove('error');
-      if (message) message.remove();
+      this.reset(el);
 
       // If this input valid, skip messaging
       if (el.validity.valid) continue;
 
-      // Create the new error message.
-      message = document.createElement('div');
-
-      // Get the error message from localized strings.
-      if (el.validity.valueMissing &&
-        this.STRINGS.VALID_REQUIRED) {
-        message.innerHTML = this.STRINGS.VALID_REQUIRED;
-      } else if (!el.validity.valid &&
-        this.STRINGS[`VALID_${el.type.toUpperCase()}_INVALID`]) {
-        let stringKey = `VALID_${el.type.toUpperCase()}_INVALID`;
-        message.innerHTML = this.STRINGS[stringKey];
-      } else {
-        message.innerHTML = el.validationMessage;
-      }
-
-      message.setAttribute('aria-live', 'polite');
-      message.classList.add('error-message');
-
-      // Add the error class and error message.
-      container.classList.add('error');
-      container.insertBefore(message, container.childNodes[0]);
+      this.highlight(el);
     }
 
-    return (validity) ? event : validity;
+    return (validity) ? this : validity;
   }
 
   /**
-   * A function to assign pre localized strings to the class.
-   * @param  {object} localizedStrings A dictionairy of strings in the format
-   *                                   'VALID_REQUIRED': 'This is required',
-   *                                   'VALID_{{ TYPE }}_INVALID': 'Invalid'
-   * @return {class}                   The object class
+   * Adds focus and blur events to inputs with required attributes
+   * @param   {object}  form  Passing a form is possible, otherwise it will use
+   *                          the form passed to the constructor.
+   * @return  {class}         The form class
    */
-  strings(localizedStrings) {
-    Object.assign(this.STRINGS, localizedStrings);
+  watch(form = false) {
+    this.FORM = (form) ? form : this.FORM;
+
+    let elements = this.FORM.querySelectorAll(Forms.selectors.REQUIRED);
+
+    /** Watch Individual Inputs */
+    for (let i = 0; i < elements.length; i++) {
+      // Remove old messaging if it exists
+      let el = elements[i];
+
+      el.addEventListener('focus', () => {
+        this.reset(el);
+      });
+
+      el.addEventListener('blur', () => {
+        if (!el.validity.valid)
+          this.highlight(el);
+      });
+    }
+
+    /** Submit Event */
+    this.FORM.addEventListener('submit', event => {
+      event.preventDefault();
+
+      if (this.valid(event) === false)
+        return false;
+
+      this.submit(event);
+    });
+
+    return this;
+  }
+
+  /**
+   * Removes the validity message and classes from the message.
+   * @param   {object}  el  The input element
+   * @return  {class}       The form class
+   */
+  reset(el) {
+    let container = el.parentNode;
+    let message = container.querySelector('.' + Forms.classes.ERROR_MESSAGE);
+
+    // Remove old messaging if it exists
+    container.classList.remove(Forms.classes.ERROR_CONTAINER);
+    if (message) message.remove();
+
+    return this;
+  }
+
+  /**
+   * Displays a validity message to the user. It will first use any localized
+   * string passed to the class for required fields missing input. If the
+   * input is filled in but doesn't match the required pattern, it will use
+   * a localized string set for the specific input type. If one isn't provided
+   * it will use the default browser provided message.
+   * @param   {object}  el  The invalid input element
+   * @return  {class}       The form class
+   */
+  highlight(el) {
+    let container = el.parentNode;
+    let message = container.querySelector('.' + Forms.classes.ERROR_MESSAGE);
+
+    // Create the new error message.
+    message = document.createElement(Forms.markup.ERROR_MESSAGE);
+
+    // Get the error message from localized strings (if set).
+    if (el.validity.valueMissing &&
+      Forms.strings.VALID_REQUIRED) {
+      message.innerHTML = Forms.strings.VALID_REQUIRED;
+    } else if (!el.validity.valid &&
+      Forms.strings[`VALID_${el.type.toUpperCase()}_INVALID`]) {
+      let stringKey = `VALID_${el.type.toUpperCase()}_INVALID`;
+      message.innerHTML = Forms.strings[stringKey];
+    } else {
+      message.innerHTML = el.validationMessage;
+    }
+
+    // Set aria attributes and css classes to the message
+    message.setAttribute(Forms.attrs.ERROR_MESSAGE[0],
+      Forms.attrs.ERROR_MESSAGE[1]);
+    message.classList.add(Forms.classes.ERROR_MESSAGE);
+
+    // Add the error class and error message to the dom.
+    container.classList.add(Forms.classes.ERROR_CONTAINER);
+    container.insertBefore(message, container.childNodes[0]);
 
     return this;
   }
@@ -134,5 +174,29 @@ class Forms {
  * }
  */
 Forms.strings = {};
+
+/** Placeholder for the submit function */
+Forms.submit = () => {};
+
+/** Classes for various containers */
+Forms.classes = {
+  'ERROR_MESSAGE': 'error-message', // error class for the validity message
+  'ERROR_CONTAINER': 'error' // class for the validity message parent
+};
+
+/** HTML tags and markup for various elements */
+Forms.markup = {
+  'ERROR_MESSAGE': 'div'
+};
+
+/** DOM Selectors for various elements */
+Forms.selectors = {
+  'REQUIRED': '[required="true"]' // Selector for required input elements
+};
+
+/** Attributes for various elements */
+Forms.attrs = {
+  'ERROR_MESSAGE': ['aria-live', 'polite'] // Attribute for valid error message
+};
 
 export default Forms;
