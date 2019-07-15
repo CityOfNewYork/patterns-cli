9784c6c6eff8b2ac3a66516f907fcb55e9bd4667
devowhippit <devonhirth@gmail.com>
2019-07-15 09:25:14 -0400

    change dev dependencies to dependencies and test PWD constant for /.bin sourcing

--- scripts/svg.js
+++ scripts/svg.js
@@ -2,13 +2,14 @@ const concurrently = require('concurrently');
 const nodemon = require('nodemon');
 const args = process.argv.slice(2);
 const prefix = '[svg]';
+const bin = `${process.env.PWD}/node_modules/.bin`;
 
 if (args.includes('-w') || args.includes('--watch')) {
   nodemon('-e svg --watch ./src/svg -x pttrn svg');
 } else {
   concurrently([
-    'node_modules/.bin/svgo -f ./src/svg -o ./dist/svg --disable=convertPathData',
-    'node_modules/.bin/svgstore -o ./dist/icons.svg ./dist/svg/*.svg --inline'
+    `${bin}/svgo -f ./src/svg -o ./dist/svg --disable=convertPathData`,
+    `${bin}/svgstore -o ./dist/icons.svg ./dist/svg/*.svg --inline`
   ], {
     prefix: prefix
   });
