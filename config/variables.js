db377be0a291870e8985efd84f254ef4aed9fd2b
devowhippit <devonhirth@gmail.com>
2019-07-17 14:45:46 -0400

    Replace json-sass with json-to-sass package

--- config/variables.js
+++ config/variables.js
@@ -6,8 +6,8 @@ const package = require('../package.json');
 const version = process.env.V || package.version;
 
 const variables = {
-  'version': version,
-  'cdn': '"https://cdn.jsdelivr.net/gh/CityOfNewYork/nyco-patterns-framework@v' + version + '/dist/"'
+  'version': `"${version}"`,
+  'cdn': `"https://cdn.jsdelivr.net/gh/CityOfNewYork/nyco-patterns-framework@v${version}/dist/"`
 };
 
 module.exports = variables;
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,7 +2,7 @@ const shell = require('shelljs');
 const args = process.argv.slice(2);
 
 if (args.includes('-w') || args.includes('--watch')) {
-  shell.exec('npx rollup  -c config/rollup.js -w');
+  shell.exec('npx rollup -c config/rollup.js -w');
 } else {
   shell.exec('npx rollup -c config/rollup.js');
 }
