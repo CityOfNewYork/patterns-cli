20bc484ee28ff8146623b00346a0f30894044f5c
devowhippit <devonhirth@gmail.com>
2020-03-13 11:59:53 -0400

    Remove optional packages (mqpacker, autoprefixer, stylelint)

--- config/postcss.js
+++ config/postcss.js
@@ -3,11 +3,8 @@
  */
 
 const package = require(`${process.env.PWD}/package.json`);
-const tailwindcss = require('tailwindcss'); // utility framework/management
-const autoprefixer = require('autoprefixer'); // adds vendor spec prefixes
-const cssnano = require('cssnano'); // modern css compiling/minification
-const mqpacker = require('css-mqpacker'); // packs media queries together
-const stylelint = require('stylelint');
+const tailwindcss = require('tailwindcss');                 // utility framework/management
+const cssnano = require('cssnano');                         // modern css compiling/minification
 
 /**
  * Config
@@ -16,10 +13,7 @@ const stylelint = require('stylelint');
 module.exports = {
   parser: 'postcss-scss',
   plugins: [
-    stylelint(package.stylelintConfig),
     tailwindcss(`${process.env.PWD}/config/tailwind.js`),
-    autoprefixer('last 4 version'),
-    mqpacker({sort: true}),
     cssnano()
   ]
 };
