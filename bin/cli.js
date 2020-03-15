b530234b6cb8c96d697a5adf7e9f2d97c581cf96
devowhippit <devonhirth@gmail.com>
2020-03-15 08:32:45 -0400

    Rename scripts to bin

--- bin/cli.js
+++ bin/cli.js
@@ -3,7 +3,7 @@
 let script = process.argv.splice(2, 1)[0];
 
 if (undefined === script) {
-  require(`./default.js`);
+  require(`${__dirname}/default.js`).run();
 } else {
   require(`${__dirname}/${script}`).run();
 }
