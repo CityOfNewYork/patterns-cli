3f61ce0f232d6f48f465bb6dd46b1ed4fb926a5b
devowhippit <devonhirth@gmail.com>
2020-03-14 22:25:51 -0400

    remove script flagging

--- scripts/cli.js
+++ scripts/cli.js
@@ -1,16 +1,9 @@
 #!/usr/bin/env node
 
 let script = process.argv.splice(2, 1)[0];
-let modules = [
-  'styles', 'tokens', 'sass', 'postcss', 'slm', 'serve', 'svgs', 'rollup', 'publish', 'lint', 'pa11y'
-];
 
 if (undefined === script) {
   require(`./default.js`);
 } else {
-  if (modules.includes(script)) {
-    require(`${__dirname}/${script}`).run();
-  } else {
-    require(`${__dirname}/${script}`);
-  }
+  require(`${__dirname}/${script}`).run();
 }
