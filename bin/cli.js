1ddb77a9004dc3e215309b7de20d63cbcab71429
devowhippit <devon@expdesign.io>
2020-11-30 09:48:06 -0500

    Support blank default (no command)

--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
-let script = process.argv.splice(2, 1)[0];
+let script = process.argv[2];
 
-if (undefined === script) {
-  require(`${__dirname}/default.js`).run();
+if (undefined === script || script[0] === '-') {
+  process.argv.splice(2, 0, 'default');
+
+  require(`${__dirname}/default`).run();
 } else {
   require(`${__dirname}/${script}`).run();
 }
