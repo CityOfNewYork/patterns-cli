d9fe13b0582ee6ab2d60489cf074c6719408b9dc
devowhippit <devonhirth@gmail.com>
2020-12-08 11:10:57 -0500

    Fix installed path resolution

--- bin/util/installed.js
+++ bin/util/installed.js
@@ -2,5 +2,5 @@ const path = require('path');
 const fs = require('fs');
 
 module.exports = (pkg) => {
-  return fs.existsSync(path.join(__dirname, '../../', 'node_modules', pkg));
+  return fs.existsSync(path.join(process.env.PWD, 'node_modules', pkg));
 };
