356f2b2d11f8aa1bd816ab8e200e36af64b6d8e9
devowhippit <devonhirth@gmail.com>
2021-11-12 10:57:59 -0500

    Rollup compatibility update (replace)

--- config/rollup.js
+++ config/rollup.js
@@ -18,6 +18,7 @@ const global = resolve('config/global');
  */
 let plugins = [
   replace({
+    'preventAssignment': true,
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
   }),
   nodeResolve.nodeResolve({
