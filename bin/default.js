eb88ae4b42e19c05dc33624c3b2f47b9177d12d4
Devon Hirth <devowhippit@users.noreply.github.com>
2022-08-22 12:44:32 -0400

    Default command; quote script path and add error handling

--- bin/default.js
+++ bin/default.js
@@ -4,48 +4,61 @@
  * Dependencies
  */
 
- const concurrently = require('concurrently');
- const resolve = require(`${__dirname}/util/resolve`);
+const concurrently = require('concurrently');
+const resolve = require(`${__dirname}/util/resolve`);
+const cnsl = require(`${__dirname}/util/console`);
 
- /**
-  * Args
-  */
+/**
+ * Args
+ */
 
- const config = resolve('config/default.js');
- const arguments = require(`${__dirname}/util/args`);
- const args = arguments.args;
- const dict = arguments.dict;
+const config = resolve('config/default.js');
+const arguments = require(`${__dirname}/util/args`);
+const args = arguments.args;
+const dict = arguments.dict;
 
- const flags = dict.map(d => (args[d.name]) ? d.flags[0] : '')
-   .filter(f => f != '').join(' ');
+const flags = dict.map(d => (args[d.name]) ? d.flags[0] : '')
+  .filter(f => f != '').join(' ');
 
- /**
-  * Constants
-  */
+/**
+ * Constants
+ */
 
- const scripts = config.commands;
- const opts = config.concurrently;
+const scripts = config.commands;
+const opts = config.concurrently;
 
- /**
-  * Main task
-  */
- const main = () => {
-   concurrently(scripts.map(s => `${__dirname}/cli.js ${s} ${flags}`), opts);
- };
+/**
+ * Main task
+ */
+const main = () => {
+  try {
+    const result = concurrently(scripts.map(s => `"${__dirname}/cli.js" ${s} ${flags}`), opts);
 
- /**
-  * Runner
-  */
- const run = () => {
-   main();
- };
+    result.then(() => {
+      cnsl.success(`pttrn command (default) finished`);
+    }, (error) => {
+      error.forEach(err => {
+        cnsl.error(`pttrn command (default) failed with exit code ${err.exitCode}: ${err.command.command}`);
+      });
+    });
+  } catch (error) {
+    cnsl.error(`pttrn command (default) failed`);
+  }
+};
 
- /**
-  * Export our methods
-  *
-  * @type {Object}
-  */
- module.exports = {
-   main: main,
-   run: run
- };
+/**
+ * Runner
+ */
+const run = () => {
+  main();
+};
+
+/**
+ * Export our methods
+ *
+ * @type {Object}
+ */
+module.exports = {
+  main: main,
+  run: run
+};
